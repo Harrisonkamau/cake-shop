7d429061d0e190f1155125d61554cca4ef42b555
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-16 20:51:39 +0300

    configure home route

--- views.py
+++ views.py
@@ -1,10 +1,10 @@
 # import Flask class from flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, abort, session
 
 # create flask app
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'F34TF$($e34D';
+app.config['SECRET_KEY'] = 'hialreuhsan.ndjfbfhgfhfhfre'
 
 
 # use decorators to link the functions to the url
@@ -12,6 +12,11 @@ app.config['SECRET_KEY'] = 'F34TF$($e34D';
 def index():
     return render_template('index.html')
 
+
+# create a home page
+@app.route('/')
+def home():
+    return render_template('layout.html')
 
 # start the server
 if __name__ == "__main__":
