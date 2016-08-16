1ac1b8efbb160e12ef7d3cf799b4c63273c8ed59
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-16 19:06:12 +0300

    config secret key

--- views.py
+++ views.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template
 # create flask app
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'F34TF$($e34D';
+
 
 # use decorators to link the functions to the url
 @app.route('/index')
