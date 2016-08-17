5e493ffcfa3424593fa878d4e194bf3f97fe2561
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-17 18:39:44 +0300

    configure username's validators

--- forms.py
+++ forms.py
@@ -49,10 +49,7 @@ class RegisterForm(Form):
 
 # create a login form
 class LoginForm(Form):
-    email = StringField('Email',validators=[DataRequired(), Email()])
+    username = StringField('Username',validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
 
 
-# create a post form
-class PostForm(Form):
-    content = TextAreaField("Post anything!", validators=[DataRequired()])
