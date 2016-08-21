88ea842267749fcc62f145283ba5e6cfda7df363
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-21 19:49:24 +0300

    implement password hash

--- app/views.py
+++ app/views.py
@@ -1,32 +1,21 @@
 from flask import Flask, g, render_template, redirect, flash, url_for, request
+from flask_bcrypt import check_password_hash, generate_password_hash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-from flask_sqlalchemy import SQLAlchemy
-import os
-from forms import RegistrationForm, LoginForm
+import sqlite3
+from models import DATABASE as db
+
+import forms
 import models
 
-
-# create a flask Constructor
+# create flask app
 app = Flask(__name__)
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 # create a secret key
-app.secret_key = os.urandom(24)
-
-# disable SQLALCHEMY_TRACK_MODIFICATIONS
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'cake.db')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-
+app.secret_key = 'auoesh.beoehgh.32.tibe.jeen'
 
 # create a login manager
 login_manager = LoginManager()
 login_manager.init_app(app)  # sets up the login manager for the app
 login_manager.login_view = 'login'
-
-# database object
-db = SQLAlchemy(app)
 
 
 # load user route
@@ -42,67 +31,91 @@ def load_user(userid):
 @app.before_request
 def before_request():
     """Connect to the database before each request"""
+    g.db = models.DATABASE
+    g.db.connect()
     g.user = current_user
-    if g.user.is_authenticated:
-        db.session.add(g.user)
-        db.session.commit()
+
+
+# create an after request route
+@app.after_request
+def after_request(response):
+    """Close the database after each request"""
+    g.db.close()
+    return response
 
 
 # create a register route to register a new user
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()
-    if request.method == 'POST' and form.validate():
-        user = models.User(username=form.username.data, email=form.email.data, password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('You can now login.')
-        return redirect(url_for('index'))
-    return render_template('register.html', form=form)
+    if request.method == 'POST':
+        if not request.form['username']:
+            error = 'You have to enter a username'
+        elif not request.form['email'] or '@' not in request.form['email']:
+            error = 'You have to enter a valid email address'
+        elif not request.form['password']:
+            error = 'You have to enter a password'
+        else:
+            print(request.form['username'])
+            db.execute('INSERT INTO User VALUES (?, ?, ?)',
+                       (request.form['username'], request.form['email'],
+                        generate_password_hash(request.form['password'])))
+            db.commit()
+            flash('You were successfully registered and can login now')
+            return render_template('layout.html')
+    return render_template('register.html')
 
 
 # create a login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if g.user is not None and g.user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
+    form = forms.LoginForm()
     if form.validate_on_submit():
-        user = models.User.query.filter_by(email=form.email.data).first()
-        login_user(user)
-        return redirect(url_for('layout'))
+        try:
+            user = models.User.get(models.User.username == form.username.data)
+        except models.DoesNotExist:
+            flash("Your username and password do not match!", "error")
+        else:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash("Successfully logged in!!", "success")
+                return redirect(url_for('home'))
+            else:
+                flash("Your emails and password do not match!", "error")
     return render_template('login.html', form=form)
 
 
 # create a logout route
-@app.route('/logout')
 @login_required
+@app.route('/logout')
 def logout():
     logout_user()
     flash("You've been logged out!", "success")
-    return redirect('/login')
+    return redirect(url_for('index'))
 
 
-# create a home route
-@app.route('/')
+# use decorators to link the functions to the url
+@app.route('/index')
 def index():
     return render_template('home.html')
 
 
-# after logging in allow the user to make purchase
-@app.route('/layout')
+# create a home page
+@app.route('/')
 @login_required
-def layout():
+def home():
     return render_template('layout.html')
-
-
-# create an error handler route
-@app.errorhandler(404)
-def not_found(e):
-    return render_template('404.html'), 404
 
 
 # start the server
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    models.initialize()
+    try:
+        models.User.create_user(
+            username='harrisonkamau',
+            email='kamauharry@yahoo.com',
+            password='password',
+            admin=True
+        )
+    except ValueError:
+        pass
+app.run(debug=True)
