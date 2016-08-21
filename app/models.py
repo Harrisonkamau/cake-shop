e01131284f3b349d0506d40926ccfaa86786a9b8
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-21 19:49:49 +0300

    catch integrity errors

--- app/models.py
+++ app/models.py
@@ -1,68 +1,38 @@
-from __init__ import app, db
-from flask_security import Security, SQLAlchemyUserDatastore, \
-    UserMixin, RoleMixin
-from hashlib import md5
+# import modules
 
-roles_users = db.Table('roles_users',
-                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+from flask_bcrypt import generate_password_hash
+from flask_login import UserMixin
+from peewee import *
 
 
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    default = db.Column(db.Boolean, default=False, index=True)
-    permissions = db.Column(db.Integer)
-    users = db.relationship('User', backref='role', lazy='dynamic')
+# set up database
+DATABASE = SqliteDatabase('cake.db')
 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    password = db.Column(db.String(255))
-    ordered_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
-    purchases = db.relationship('Purchase', backref='author', lazy='dynamic')
-    about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime)
+# create a users' model
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
 
-    def avatar(self, size):
-        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
+    class Meta:
+        database = DATABASE
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    @classmethod  # without this, a user instance has to be created to call create_user to create a user instance!
+    def create_user(cls, username, email, password, admin=False):
+        try:  # cls refers to the User class
+            with DATABASE.transaction():
+                cls.create(
+                    username=username,
+                    email=email,
+                    password=generate_password_hash(password)
+                           )
 
-    @property
-    def is_authenticated(self):
-        return True
-
-    @property
-    def is_active(self):
-        return True
-
-    @property
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        try:
-            return unicode(self.id)
-        except NameError:
-            return str(self.id)
+        except IntegrityError:
+            raise ValueError("User already exists!")
 
 
-class Purchases(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
-
-
-
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User], safe=True)
+    DATABASE.close()
