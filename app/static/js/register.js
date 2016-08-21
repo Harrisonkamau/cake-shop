23a6741c7fe8905062b5d2ff32f78059b663e15e
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-21 14:28:47 +0300

    validate user input

---
+++ app/static/js/register.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    // Generate a simple captcha
+
+if ($("#password").val() != $("#confirmpassword").val()) {
+          alert("Passwords do not match.");
+      }
+    $('#signupform').formValidation({
+        framework: 'bootstrap',
+
+        fields: {
+
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 30,
+                        message: 'The username must be more than 6 and less than 30 characters long'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_\.]+$/,
+                        message: 'The username can only consist of alphabetical, number, dot and underscore'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'The email address is required'
+                    },
+                    emailAddress: {
+                        message: 'The input is not a valid email address'
+                    }
+                }
+            },
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: 'The password is required'
+                    },
+                    different: {
+                        field: 'username',
+                        message: 'The password cannot be the same as username'
+                    }
+                }
+            }
+
+        }
+    });
+});
